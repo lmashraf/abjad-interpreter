22e3edc05c3791723a65871199ea1b078d6cfda4
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2016-08-31 18:09:01 +0200

    Update SymbolToken and BaseToken

--- AbjadInterpreter/BaseToken.h
+++ AbjadInterpreter/BaseToken.h
@@ -9,15 +9,14 @@
 /** All tokens must derive from this token type. **/
 class BaseToken
 {
-private:
-    TypeOfToken TokenType;
-
 public:
     BaseToken( TypeOfToken token );
 
-public:
     TypeOfToken GetType( );
     virtual const char* ParseToken( const char* s ) = 0;
+
+private:
+    TypeOfToken TokenType;
 };
 
 //--------------------------------------------------------------------------------------------------
--- AbjadInterpreter/SymbolToken.h
+++ AbjadInterpreter/SymbolToken.h
@@ -10,6 +10,13 @@ class SymbolToken : public BaseToken
 public:
     SymbolToken( );
     ~SymbolToken( );
+
+public:
+    const char* ParseToken(const char* s);
+    const char* GetSymbol(const char* s);
+
+private:
+    std::string Symbol;
 };
 
 //--------------------------------------------------------------------------------------------------
